9b3203c710d644c1ebe44dbc777d0dfc3f842da1
sivroni <sivroni@mail.tau.ac.il>
2017-02-06 13:00:14 +0200

    updated

--- new version/kci.h
+++ new version/kci.h
@@ -15,20 +15,12 @@
 #define IOCTL_SET_FD _IOW(MAJOR_NUM, 1, unsigned long)
 #define IOCTL_CIPHER _IOW(MAJOR_NUM, 2, unsigned long)
 
-#define LOF_FILE_PATH "/sys/kernel/debug/kcikmod/calls"
+
 #define DEVICE_RANGE_NAME "kci_kmod"
+#define DEVICE_FILE_PATH "/dev/kci_dev" // device file path
 #define KCIKMOD "kcikmod"
-#define CALLS "calls"
 
 
-///// define commands: /////
-#define INIT_STR "-init"
-#define PID_STR "-pid"
-#define FD_STR "-fd"
-#define START_STR "-start"
-#define STOP_STR "-stop"
-#define RM_STR "-rm"
-////////////////////////////
 
 #define SUCCESS 0
 
